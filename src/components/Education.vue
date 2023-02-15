69783536d75f883f0b75fa47ec8c874719c8340b
Magnuz21 <corpuzadrianne676@gmail.com>
2023-02-16 03:28:02 +0800

    first commit

---
+++ src/components/Education.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="progress-journey flex justify-center mt-10 transition-all">
+    <ul class="journey-description flex flex-col w-96 text-right">
+      <li class="pr-10">
+        <h2 class="text-sm dark:text-white">
+          Bachelor of Science in Information Technology - Holy Cross of
+          Davao College
+        </h2>
+        <span class="text-xs f-gray"> 2019 – Ongoing</span>
+      </li>
+      <li></li>
+    </ul>
+    <ul class="journey flex flex-col w-12 lg:w-5 items-center">
+      <li></li>
+      <li></li>
+    </ul>
+    <ul class="journey-description flex flex-col w-96">
+      <li></li>
+      <li class="pl-10">
+        <h2 class="text-sm dark:text-white">
+          Senior High School - Paquibato National High
+          School
+        </h2>
+        <span class="text-xs f-gray"> 2017 – 2019</span>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/Work.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="progress-journey flex justify-center mt-10 transition-all">
+    <ul class="journey-description flex flex-col w-96 text-right">
+      <li class="pr-10" data-aos="fade-right" data-aos-duration="500">
+        <h2 class="text-sm dark:text-white">Doing Freelancing</h2>
+        <span class="text-xs f-gray"> Current</span>
+      </li>
+      <li></li>
+      <li class="pr-10" data-aos="fade-right" data-aos-duration="500">
+        <h2 class="text-sm dark:text-white">
+          Frontend Developer at Innovations Groups
+        </h2>
+        <span class="text-xs f-gray"> 02/2023 – 04/2023</span>
+      </li>
+      <li></li>
+      <li class="pr-10" data-aos="fade-right" data-aos-duration="500">
+        <h2 class="text-sm dark:text-white">
+          Web/Mobile Developer at Saample
+        </h2>
+        <span class="text-xs f-gray"> 07/2022 – 06/2023</span>
+      </li>
+    </ul>
+    <ul class="journey flex flex-col w-12 lg:w-5 items-center">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <ul class="journey-description flex flex-col w-96">
+      <li></li>
+      <li class="pl-10" data-aos="fade-left" data-aos-duration="500">
+        <h2 class="text-sm dark:text-white">Frontend Developer at Sample</h2>
+        <span class="text-xs f-gray"> 05/2022 – 08/2023</span>
+      </li>
+      <li></li>
+      <li class="pl-10" data-aos="fade-left" data-aos-duration="500">
+        <h2 class="text-sm dark:text-white">
+          Software Engineer at Sample
+        </h2>
+        <span class="text-xs f-gray"> 06/2019 – 01/2023</span>
+      </li>
+      <li></li>
+    </ul>
+  </div>
+</template>
